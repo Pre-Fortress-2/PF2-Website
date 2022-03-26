b0fb8bcbc6bc03471f7cf1df9adcc255b4b17dbc
dangreene0 <35577693+dangreene0@users.noreply.github.com>
2022-03-26 03:22:11 -0400

    Updated WIP installation page

---
+++ scripts/game-os-pagination.js
@@ -0,0 +1,98 @@
+
+// Declarations to shorten referencing later.
+const btn_win_select = document.getElementById("btn_win_select");
+const btn_mac_select = document.getElementById("btn_mac_select");
+const btn_linux_select = document.getElementById("btn_linux_select");
+
+const btn_win_dl = document.getElementById("btn_win_dl");
+const btn_mac_dl = document.getElementById("btn_mac_dl");
+const btn_linux_dl = document.getElementById("btn_linux_dl");
+
+const btn_back = document.getElementById("btn_back");
+
+// Back button that disables itself and current download buttons and displays select buttons
+function backPage()
+{
+    btn_back.style.display = "none";
+
+    btn_win_dl.style.display = "none";
+    btn_mac_dl.style.display = "none";
+    btn_linux_dl.style.display = "none";
+
+    btn_win_select.style.display = "inline-block";
+    btn_mac_select.style.display = "inline-block";
+    btn_linux_select.style.display = "inline-block";
+}
+
+// Called when an OS is selected
+function showBackBtn()
+{
+    btn_back.style.display = "inline-block";
+
+    btn_win_select.style.display = "none";
+    btn_mac_select.style.display = "none";
+    btn_linux_select.style.display = "none";
+}
+
+// Lets the user select the OS
+function winSelect()
+{
+    btn_win_dl.style.display = "inline-block";
+    showBackBtn()
+}
+
+function macSelect()
+{
+    btn_mac_dl.style.display = "inline-block";
+    showBackBtn()
+}
+
+function linuxSelect()
+{
+    btn_linux_dl.style.display = "inline-block";
+    showBackBtn()
+}
+
+// These functions actually take the user to the installation.
+// TODO flesh out installation instructions.
+function installWin()
+{
+    window.open("https://drive.google.com/file/d/1amkUEPJ6CZDyamvPivDl0RlmU_6cUm6N/view");
+}
+
+function installMac()
+{
+    window.open("https://yoinkysploinky.xyz");
+}
+
+function installLinux()
+{
+    window.open("https://twitter.com/home");
+}
+
+// Hides all buttons except back button and then shows the detected option
+window.onload = function() 
+{
+    btn_back.style.display = "inline-block";
+
+    btn_win_dl.style.display = "none";
+    btn_mac_dl.style.display = "none";
+    btn_linux_dl.style.display = "none";
+
+    btn_win_select.style.display = "none";
+    btn_mac_select.style.display = "none";
+    btn_linux_select.style.display = "none";
+
+    if( navigator.platform.includes("Win") )
+    {
+        btn_win_dl.style.display = "inline-block";
+    }
+    if( navigator.platform.includes("Mac") )
+    {
+        btn_mac_dl.style.display = "inline-block";
+    }
+    if( !navigator.platform.includes("Mac") && !navigator.platform.includes("Win") ) 
+    {
+        btn_mac_dl.style.display = "inline-block";
+    }
+};
