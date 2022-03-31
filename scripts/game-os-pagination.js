cb3b674ba8d57904490bc8ab81207c98aedea0b5
dangreene0 <35577693+dangreene0@users.noreply.github.com>
2022-03-31 04:17:49 -0400

    Fixed broken OS hash links

--- scripts/game-os-pagination.js
+++ scripts/game-os-pagination.js
@@ -17,8 +17,9 @@ const choose_game_area = document.getElementById("choose-game-area");
 
 
 const first_mirror = "https://drive.google.com/file/d/1FUUfHo5NbxvCNsKDUiZVxU4x8q8NkaKk/view"
+const all_in_one = "https://drive.google.com/file/d/1amkUEPJ6CZDyamvPivDl0RlmU_6cUm6N/view"
 
-const browser_hash = window.location.hash.split('#')[1].toLowerCase();
+const browser_hash = window.location.hash.split('#')[1];
 
 // Back button that disables itself and current download buttons and displays select buttons
 
@@ -38,8 +39,8 @@ function windows_onlyRender(state)
 
 function backPage()
 {
-    choose_game_area.style.height = "50em"
-    btn_back.style.top = "70%";
+    // btn_back.style.top = "70%"
+    // choose_game_area.style.height = "50em"
 
     btn_back.style.display = "none";
 
@@ -67,8 +68,8 @@ function showBackBtn()
 }
 function everyoneElse()
 {
-    btn_back.style.top = "82%";
-    choose_game_area.style.height = "70em"
+    // btn_back.style.top = "82%"
+    // choose_game_area.style.height = "70em"
     everyone_else.style.display = "block";
     all_versions.style.display = "block"
     
@@ -99,7 +100,7 @@ function linuxSelect()
 // TODO flesh out installation instructions.
 function installWin()
 {
-    window.open("https://drive.google.com/file/d/1amkUEPJ6CZDyamvPivDl0RlmU_6cUm6N/view");
+    window.open(all_in_one);
 }
 
 function installMac()
@@ -115,24 +116,8 @@ function installLinux()
 // Hides all buttons except back button and then shows the detected option
 window.onload = function() 
 {
-
-    if (browser_hash == "windows"){
-        backPage()
-        winSelect()
-        return
-    }
-    if (browser_hash == "mac"){
-        backPage()
-        macSelect()
-        return
-    }
-    if (browser_hash == "linux"){
-        backPage()
-        linuxSelect()
-        return
-    }
-   
     backPage()
+
     if( navigator.platform.includes("Win")  ) 
     {
         winSelect()
@@ -143,6 +128,19 @@ window.onload = function()
     }
     if( !navigator.platform.includes("Mac") && !navigator.platform.includes("Win") ) 
     {
+        linuxSelect()
+    }
+
+    if (browser_hash.toLowerCase() == "windows"){
+        backPage()
+        winSelect()
+    }
+    if (browser_hash.toLowerCase() == "mac"){
+        backPage()
+        macSelect()
+    }
+    if (browser_hash.toLowerCase() == "linux"){
+        backPage()
         linuxSelect()
     }
 };
