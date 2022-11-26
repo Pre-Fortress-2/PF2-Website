a3c3475c89493a911e869bdd3a0e44415b84296b
dead_thing <50424109+dead-thing@users.noreply.github.com>
2022-11-25 19:55:10 -0500

    updated the download buttons to the hotfix

--- scripts/game-os-pagination.js
+++ scripts/game-os-pagination.js
@@ -16,8 +16,8 @@ const linux_only = document.getElementsByClassName("linux_only")
 const all_versions = document.getElementById("all_versions");
 const choose_game_area = document.getElementById("choose-game-area");
 
-const first_mirror = "https://github.com/Pre-Fortress-2/pf2/releases/tag/0.7"
-const all_in_one = "https://github.com/Pre-Fortress-2/pf2/releases/tag/0.7"
+const first_mirror = "https://github.com/Pre-Fortress-2/pf2/releases/tag/0.7hotfix"
+const all_in_one = "https://github.com/Pre-Fortress-2/pf2/releases/tag/0.7hotfix"
 
 const browser_hash = window.location.hash.split('#')[1];
 
